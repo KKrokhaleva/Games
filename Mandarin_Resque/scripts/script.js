b709898f28c5ba56ceb3b3facf4e9046960e9279
KKrokhaleva <kkrohaleva93@gmail.com>
2022-01-15 21:14:41 +0200

    fixed mistake and added restart button

--- Mandarin_Resque/scripts/script.js
+++ Mandarin_Resque/scripts/script.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const icon = document.querySelector('#icon');
 const mandarinsAmountElement = document.querySelector('#mandarinsAmount');
@@ -6,78 +6,84 @@ const startButton = document.querySelector('#startButton');
 const gameTime = document.querySelector('#gameTime');
 const mandarin = document.querySelector('#mandarin');
 const grinch = document.querySelector('#grinch');
+const restartButton = document.querySelector('#restartButton');
 
+
+let gameInterval;
 let mandarinsAmount = 0;
 let gameDuration = 15;
-let gameInterval;
 let mandarinInterval;
-let grinchInterval;
 let grinchIntervalDelay;
 
-icon.addEventListener('click',() => {
+mandarin.addEventListener('click', event => {
     mandarinsAmount++;
-    mandarinsAmountElement.textContent = `You caught ${mandarinsAmount} mandarin`;
+    mandarinsAmountElement.textContent = `You caught ${mandarinsAmount} mandarins`;
     setMandarinPosition();
     clearInterval(mandarinInterval);
     mandarinInterval = setInterval(setMandarinPosition, 1000);
 });
 
-grinch.addEventListener('click', () =>{
+grinch.addEventListener('click', () => {
     gameDuration = 15;
-    gameTime.textContent ='Game over!';
+    gameTime.textContent = `Game over!`;
     icon.style.display = 'none';
     clearInterval(mandarinInterval);
-    clearInterval (gameInterval);
+    clearInterval(gameInterval);
     mandarinsAmount--;
-    mandarinsAmountElement.textContent = `You caught ${mandarinsAmount} mandarin`;
-})
+    mandarinsAmountElement.textContent = `You caught ${mandarinsAmount} mandarins`;
+    startButton.removeAttribute('disabled');
+});
 
-function resetGame() {
+function resetGame () {
     gameDuration = 15;
     mandarinsAmount = 0;
-    mandarinsAmountElement.textContent = `You caught ${mandarinsAmount} mandarin`;
-    gameTime.textContent = `${gameDuration} seconds left`;
+    mandarinsAmountElement.textContent = `You caught ${mandarinsAmount} mandarins`;
+    gameTime.textContent = `${gameDuration} seconds left`
 }
 
- function setMandarinPosition() {
-    icon.style.left = Math.round(Math.random()*274) + 'px ';
-    icon.style.top = Math.round(Math.random()*274) + 'px ';
+function setMandarinPosition () {
+    icon.style.left = Math.round(Math.random() * 274) + 'px';
+    icon.style.top = Math.round(Math.random() * 274) + 'px';
 
-    if(gameDuration === grinchIntervalDelay){
+    if(gameDuration === grinchIntervalDelay) {
         mandarin.style.display = 'none';
         grinch.style.display = 'block';
-        grinchIntervalDelay = Math.round( Math.random() * gameDuration );
-        console.log(grinchIntervalDelay);
+        grinchIntervalDelay = Math.round(Math.random() * gameDuration);
     } else {
         mandarin.style.display = 'block';
         grinch.style.display = 'none';
     }
 }
 
-function startGame() {
-    if (gameDuration === 1) {
+function gameCount () {
+    if(gameDuration === 1) {
         icon.style.display = 'none';
-        gameTime.textContent = `Game Over!`;
-        clearInterval (gameInterval);
-        clearInterval (mandarinInterval);
+        gameTime.textContent = `Game over!`;
+        clearInterval(gameInterval);
+        clearInterval(mandarinInterval);
+        startButton.removeAttribute('disabled');
         return;
     }
 
+    // todo: высчитать координату независимую от ширины холста
+
     gameDuration -= 1;
     gameTime.textContent = `${gameDuration} seconds left`;
-
 }
 
-function startGameNew() {
+function startGame() {
+    startButton.setAttribute('disabled', 'disabled');
     setMandarinPosition();
     icon.style.display = 'block';
     resetGame();
-    grinchIntervalDelay = Math.round( Math.random() * gameDuration );
-    console.log(grinchIntervalDelay);
+    grinchIntervalDelay = Math.round(Math.random() * gameDuration);
     mandarinInterval = setInterval(setMandarinPosition, 1000);
-    gameInterval = setInterval (startGame, 1000);
-    startButton.removeEventListener('click', startGameNew);
+    gameInterval = setInterval(gameCount, 1000);
 }
+startButton.addEventListener('click', startGame);
 
-startButton.addEventListener('click', startGameNew);
-
+restartButton.addEventListener('click', event => {
+    clearInterval(gameInterval);
+    clearInterval(mandarinInterval);
+    startGame()
+});
